5fafb0855afbacc318ba1915d8d3951ce81379f2
Vikash Sah <mevkashsah@gmail.com>
2021-03-23 17:55:24 +0545

    sendgrid token removed

--- helper/emailHelper.js
+++ helper/emailHelper.js
@@ -1,13 +1,10 @@
 const sgMail = require("@sendgrid/mail");
 
 const sendMail = {};
-const sendGridKey =
-  process.env.SENDGRID_API_KEY ||
-  "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";
+const sendGridKey = process.env.SENDGRID_API_KEY || "sendgrid key";
 
 sendMail.send = async (msg) => {
   sgMail.setApiKey(sendGridKey);
-
   try {
     await sgMail.send(msg);
   } catch (error) {
